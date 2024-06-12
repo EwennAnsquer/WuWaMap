5e2b819891b12d641d4225f45823d6364c3b039d
EwennAnsquer <azbyansquer@gmail.com>
2024-06-12 22:28:49 +0200

    Ajout des importations des interfaces, changement de la fonction toString et ajout des méthodes distanceTo, createPolyline et distanceToAllMobs

--- src/class/tp.ts
+++ src/class/tp.ts
@@ -1,14 +1,19 @@
-import { Icon } from 'leaflet'
+import { Icon, LatLngExpression, LatLngTuple } from 'leaflet'
 
-export default class TP {
+import markerInterface from '../interface/markerInterface';
+import tpInterface from '../interface/tpInterface';
 
-    public static tpColl:TP[] = [];
+import mob from './mob'
+import line from './line'
 
-    //x corespond à la coordonnées horizontale
-    //y corespond à la coordonnées verticale
-    //x corespond à la hauteur
-    constructor(public imageLink:string, public x:number, public y:number, public z:number){
-        TP.tpColl.push(this);
+export default class TP implements markerInterface,tpInterface{
+
+    public static coll:TP[] = [];
+    public id:number;
+
+    constructor(public type:typeTp, public imageLink:string, public x:number, public y:number, public z:number){
+        this.id=TP.coll.length;
+        TP.coll.push(this);
     }
 
     public createIcon() {
@@ -20,7 +25,27 @@ export default class TP {
         return icon;
     }
 
+    public createPolyline(mob:mob): LatLngExpression[] {
+        return [
+            [this.x, this.y] as LatLngTuple,
+            [mob.x, mob.y] as LatLngTuple
+        ]
+    }
+
+    public distanceTo(mob:mob){
+        const dx = this.x - mob.x;
+        const dy = this.y - mob.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public distanceToAllMobs(){
+        mob.coll.forEach(m => {
+            console.log(m.toString(),this.distanceTo(m))
+            new line(this.createPolyline(m))
+        });
+    }
+
     public toString():string {
-        return "TP at x:"+this.x+" y:"+this.y+" z:"+this.z+", imageLink:"+this.imageLink
+        return this.id+" "+this.type+" at x:"+this.x+" y:"+this.y+" z:"+this.z+", imageLink:"+this.imageLink
     }
 }
