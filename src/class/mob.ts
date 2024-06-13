8b962b8d24689c0da244ea5aeec1db92b6fbd3ce
EwennAnsquer <azbyansquer@gmail.com>
2024-06-13 22:14:29 +0200

    ajout de nouvelle méthode

--- src/class/mob.ts
+++ src/class/mob.ts
@@ -3,7 +3,10 @@ import { Icon, LatLngExpression, LatLngTuple } from 'leaflet'
 import markerInterface from '../interface/markerInterface';
 import mobInterface from '../interface/mobInterface';
 
-export default class mob implements markerInterface,mobInterface{
+import TP from './tp';
+import marker from './marker'
+
+export default class mob implements marker,markerInterface,mobInterface{
 
     public static coll:mob[] = [];
     public id:number;
@@ -11,6 +14,7 @@ export default class mob implements markerInterface,mobInterface{
     constructor(public nom:string, public imageLink:string, public x:number, public y:number, public z:number, public cost:cost, public element:element){
         this.id=mob.coll.length;
         mob.coll.push(this);
+        marker.coll.push(this);
     }
 
     public createIcon() {
@@ -22,17 +26,76 @@ export default class mob implements markerInterface,mobInterface{
         return icon;
     }
 
-    public createPolyline(mob:mob): LatLngExpression[] {
+    public createPolyline(mob:mob):LatLngExpression[] {
         return [
             [this.x, this.y] as LatLngTuple,
             [mob.x, mob.y] as LatLngTuple
         ]
     }
 
-    public distanceTo(mob:mob){
-        const dx = this.x - mob.x;
-        const dy = this.y - mob.y;
+    public distanceTo(e:mob | TP):number{
+        const dx = this.x - e.x;
+        const dy = this.y - e.y;
         return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public slopeDistanceTo(e:mob | TP):number { //permet de calculer la distance entre deux points en prenant en compte la différence de hauteur
+        const dx = this.x - e.x;
+        const dy = this.y - e.y;
+        const dz = this.z - e.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public slopePercentageTo(e:mob | TP):number { //permet de calculer la raideur de la pente entre deux points
+        const horizontalDistance = this.distanceTo(e);
+        const heightDifference = this.z - e.z;
+        return (heightDifference / horizontalDistance) * 100;
+    }
+
+    public distanceToAll():number[]{
+        let table:number[] = [];
+        TP.coll.forEach(tp => {
+            table.push(this.distanceTo(tp))
+        })
+        mob.coll.forEach(m => {
+            if(m == this){
+                table.push(-1)
+            }else{
+                table.push(this.distanceTo(m))
+            }
+        });
+        return table;
+    }
+
+    public slopePercentageToAll():number[]{
+        let table:number[] = [];
+        TP.coll.forEach(tp => {
+            table.push(this.slopePercentageTo(tp))
+        })
+        mob.coll.forEach(m => {
+            if(m == this){
+                table.push(-1)
+            }else{
+                table.push(this.slopePercentageTo(m))
+            }
+        })
+        return table;
+    }
+
+    public static createDistanceMatrice(){
+        let table:number[][] = [];
+        mob.coll.forEach(m => table.push(m.distanceToAll()))
+        return table;
+    }
+
+    public static createSlopePercentageMatrice(){
+        let table:number[][] = [];
+        mob.coll.forEach(m => table.push(m.slopePercentageToAll()))
+        return table;
+    }
+
+    public static getMobById(id:number){
+        return mob.coll[id];
     }
 
     public toString():string {
