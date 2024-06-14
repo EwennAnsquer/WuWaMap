6304a200c415154f68dd9ab2480c7151a09c07ad
EwennAnsquer <azbyansquer@gmail.com>
2024-06-14 19:29:07 +0200

    simplification de la création de line et ajout directement des objets concernés dans les tableaux linkTo et linkFrom

--- src/class/line.ts
+++ src/class/line.ts
@@ -1,6 +1,9 @@
-import { LatLngExpression } from 'leaflet'
+import { LatLngExpression, LatLngTuple } from 'leaflet'
 
 import lineInterface from "../interface/lineInterface"
+
+import TP from './tp';
+import mob from './mob';
 
 export default class line implements lineInterface{
     public static coll:line[] = [];
@@ -10,9 +13,22 @@ export default class line implements lineInterface{
     public static lineCap = "round";
     public static lineJoin = "round";
     public id:number;
+    public positions: LatLngExpression[] = [];
 
-    constructor(public positions:LatLngExpression[]){
+    constructor(public elTo:mob|TP, public elFrom:mob){
         this.id=line.coll.length;
         line.coll.push(this);
+        this.createPolyline();
+        this.writeLinks()
+    }
+
+    public createPolyline() {
+        this.positions.push([this.elTo.x, this.elTo.y] as LatLngTuple)
+        this.positions.push([this.elFrom.x, this.elFrom.y] as LatLngTuple)
+    }
+
+    public writeLinks(){
+        this.elTo.linkTo.push(this.elFrom)
+        this.elFrom.linkFrom.push(this.elTo)
     }
 }
