338018d1814188554cb1db0ca9e10c6c6358abe9
LENOVOLEGIONEA\Azby <azbyansquer@gmail.com>
2024-10-23 10:26:30 +0200

    Ajout de commentaires

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,10 +1,8 @@
-import { MapContainer, TileLayer, Marker, useMapEvents, Polyline, Popup, useMap, LayerGroup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, useMapEvents, Polyline, Popup } from 'react-leaflet'
 
 import './map.css'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
-
-// import { useEffect } from 'react'
 
 import mob from '../../class/mob'
 import TP from '../../class/tp'
@@ -12,8 +10,6 @@ import line from '../../class/line'
 import marker from '../../class/marker'
 
 import '../../data'
-
-import pathInterface from '../../interface/pathInterface'
 
 function map() {
 
@@ -29,58 +25,66 @@ function map() {
 
     marker.createMatrice()
 
-    function findNearestNeighborPath(cities: mob[], start: TP, unvisited: mob[]) {	
+    // Function to find the nearest path through mobs from a starting point
+    function findNearestMobPath(start: TP, unvisited: mob[]) {	
         let path: (TP|mob)[] = [start];
         let current: TP|mob = start;
         let totalDistance = 0;
 
         while (unvisited.length > 0) {
-            let nextCity = unvisited.reduce((nearest, city) => {
+            // Find the nearest unvisited mob
+            let nextMob = unvisited.reduce((nearest, city) => {
                 return marker.distanceMatrice[current.id][city.id] < marker.distanceMatrice[current.id][nearest.id] ? city : nearest;
             }, unvisited[0]);
 
-            if (marker.distanceMatrice[current.id][nextCity.id] > marker.distanceMatrice[start.id][nextCity.id]) {
+            // Break if the next mob is farther from the current point than from the start
+            if (marker.distanceMatrice[current.id][nextMob.id] > marker.distanceMatrice[start.id][nextMob.id]) {
                 break;
             }
 
-            unvisited = unvisited.filter(city => city !== nextCity);
-            path.push(nextCity);
-            totalDistance += marker.distanceMatrice[current.id][nextCity.id];
-            current = nextCity;
+            // Remove the visited mob from the unvisited list
+            unvisited = unvisited.filter(mob => mob !== nextMob);
+            path.push(nextMob);
+            totalDistance += marker.distanceMatrice[current.id][nextMob.id];
+            current = nextMob;
         }
 
         return {path, totalDistance};
     }
 
+    // Iterate through all TPs (teleport points)
     TP.coll.forEach(tp => {
         let unvisited = tp.getNearestMobs();
         const paths = [];
         let maxIterations = 1000; // Safeguard against infinite loops
         let iteration = 0;
 
+        // Find paths until all mobs are visited or max iterations reached
         while (unvisited.length > 0 && iteration < maxIterations) {
-            let {path, totalDistance} = findNearestNeighborPath(unvisited, tp, [...unvisited]);
+            let {path, totalDistance} = findNearestMobPath(tp, [...unvisited]);
             paths.push({path, totalDistance});
-            unvisited = unvisited.filter(city => !path.includes(city));
+            // Remove visited mobs from the unvisited list
+            unvisited = unvisited.filter(mob => !path.includes(mob));
             iteration++;
         }
 
+        // Check if max iterations were reached (possible infinite loop)
         if (iteration === maxIterations) {
             console.error('Max iterations reached. Possible infinite loop detected.');
         }
     
-        //Link all TPs and mobs in the paths
+        // Link all TPs and mobs in the paths
         paths.forEach(({ path }) => {
             for (let i = 0; i < path.length - 1; i++) {
-                const pointA = path[i];
-                const pointB = path[i + 1];
+                const begin = path[i];
+                const end = path[i + 1];
                 
-                // Create a new line between pointA and pointB
-                new line(pointA,pointB);
+                // Create a new line between begin and end points
+                new line(begin,end);
 
                 // Update the link matrix
-                marker.linkMatrice[pointA.id][pointB.id]++;
-                marker.linkMatrice[pointB.id][pointA.id]++;
+                marker.linkMatrice[begin.id][end.id]++;
+                marker.linkMatrice[begin.id][end.id]++;
             }
         });
     })
